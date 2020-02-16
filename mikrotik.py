6f20d7021b8414ef4b959aec7d1a98c0e7e21ab6
zaharochka92 <58701966+zaharochka92@users.noreply.github.com>
2020-02-16 22:52:07 +0300

    Update mikrotik.py
    fixed output

--- mikrotik.py
+++ mikrotik.py
@@ -1,15 +1,18 @@
 import netmiko
 from pprint import pprint
 
-# выполнение одной комманды из списка commands=[,,,,]
+# выполнение одной комманды из списка commands=[,,,,] это модифицированный чужой код
 def mikrotik_cmd(ip):
     dev={'device_type':'cisco_ios','username':'admin+ct80h','password':'zakharov1992','verbose':True,'ip':ip}
     commands = '/system resource print'
     with  netmiko.ConnectHandler(**dev,default_enter="\n\r") as ssh:
 
           result=ssh.send_command(commands)
-    return result
+    result1=''
+    result = result.split('\n')
 
-
-
+    for line in result:
+        result1+=' '.join(line.split())
+        result1+='\n'
+    return result1
 
