dcf5ff7f721089911f882d775e30d82e040f71d7
zaharochka92 <58701966+zaharochka92@users.noreply.github.com>
2020-03-09 20:31:06 +0300

    Update teletokens.py

--- teletokens.py
+++ teletokens.py
@@ -5,4 +5,4 @@ user_mikro = 'admin+ct80h'
 password_mikro = 'zakharov1992'
 arubalogin = 'AWI-256466'
 arubapassword = 'jqoYS212-j"'
-openweather_token = "11231241"
+openweather_token = 'dfc68f338cf3ef8e1a48ae3741312cc4'
