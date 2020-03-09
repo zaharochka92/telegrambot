17984598990d5b2847fb6fa6602f7bf84f4ebf5c
OlegZ <58701966+zaharochka92@users.noreply.github.com>
2020-03-09 21:00:05 +0300

    Delete teletokens.py

--- teletokens.py
+++
@@ -1,9 +0,0 @@
-teletoken = '926769897:AAEJPSp4BOrg5ba_iwobt2zkbIgT9F1dtIc' #tokens telegram
-ip_mikro_home = '192.168.42.100'
-ip_mikro_dacha = '192.168.42.200'
-user_mikro = 'admin+ct80h'
-password_mikro = 'zakharov1992'
-arubalogin = 'AWI-256466'
-arubapassword = 'jqoYS212-j"'
-openweather_token = 'dfc68f338cf3ef8e1a48ae3741312cc4'
-112
